ca0dae9500ea88af947ca5cf97dc14b710db30e3
Simon Lee <great.simonlee@gmail.com>
2022-05-31 13:21:40 -0400

    main logos

---
+++ wp-content/themes/ChinaMovesUSA/footer.php
@@ -0,0 +1,23 @@
+<br>
+<br>
+<br>
+<h2>THIS IS FOOTER</h2>
+
+        <!-- Firebase JS -->
+        <!-- <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-auth.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-firestore.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.6.5/firebase-storage.js"></script> -->
+        <!-- MAPBOX JS -->
+        <!-- <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
+        <script>
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZ3JlYXQtc2ltb25sZWUiLCJhIjoiY2t6b2szZ2U5NjA0ZjJwdHZiY3Q1OHNtcyJ9.qdLhsMCScxxfzT9CYywRUQ';
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/@turf/turf@6/turf.min.js"></script>
+        <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.min.js"></script>
+        <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.css" type="text/css"> -->
+        <!-- EMAIL JS -->
+        <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script> -->
+
+    </body>
+</html>
---
+++ wp-content/themes/ChinaMovesUSA/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php wp_head();?>
+        <!-- <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' /> -->
+    </head>
+    <body <?php body_class();?>>
+    <nav class="gnav">
+      <section class="gnav-bar">
+        <a href="<?php echo site_url('/') ?>" class="gnav-title"><img class="gnav-logo" src="<?php echo get_theme_file_uri('/img/logo/china moves logo(white w_ trans)_937x97.png') ?>" alt="ChinaMovesUSA"></a>
+        <!-- <p class="gnav-title">CHINA MOVES USA</p> -->
+        <ul class="gnav-ul">
+          <li><a href="#">首页</a></li>
+          <li><a href="#">租房</a></li>
+          <li><a href="#">找室友</a></li>
+          <li><a href="#">团队</a></li>
+          <li><a href="#">关于我们</a></li>
+          <li><a href="#">发布房源</a></li>
+        </ul>
+        <div class="login-signup">
+          <a href="#">注册</a>
+          <a href="#" class="gnav-signup">登陆</a>
+        </div>
+      </section>
+    </nav>
+
